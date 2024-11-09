39d8449d327b0a265c4a0e9983f20f14c15ce6df
Bernardo <ybernardo2015@gmail.com>
2024-11-09 15:25:10 -0300

    feat: logger for the sft9001 service

--- src/sft9001/sft9001.service.ts
+++ src/sft9001/sft9001.service.ts
@@ -1,15 +1,17 @@
 import { SFT9001 } from './interfaces/sft9001.interface';
 import { parseLocationPayload } from './utils/utils';
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class Sft9001Service {
+  private readonly logger = new Logger('SFT9001');
   private locationData = new Map<number, SFT9001>();
 
   saveLocationData(deviceId: number, payload: string) {
     const location = parseLocationPayload(deviceId, payload);
     this.locationData.set(deviceId, location);
+    this.logger.log('Location added to database');
   }
 
   getLocationData(deviceId: number): SFT9001 | undefined {
