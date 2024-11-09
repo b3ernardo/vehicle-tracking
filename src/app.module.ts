fb05f775d2e8c8993846287a3e915140447775e3
Bernardo <ybernardo2015@gmail.com>
2024-11-08 22:15:32 -0300

    feat: initial nestjs setup

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { Module } from '@nestjs/common';
+
+@Module({
+  controllers: [AppController],
+  imports: [],
+  providers: [AppService],
+})
+export class AppModule {}
